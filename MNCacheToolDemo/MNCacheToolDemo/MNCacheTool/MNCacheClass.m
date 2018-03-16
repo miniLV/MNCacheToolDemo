1f987120192e683ae7ec379011de0b0fd648b150
miniLV <liangyuhangApple@gmail.com>
2018-03-16 10:11:01 +0800

    fix - NSUserDefault save ModelDict crash

--- MNCacheToolDemo/MNCacheToolDemo/MNCacheTool/MNCacheClass.m
+++ MNCacheToolDemo/MNCacheToolDemo/MNCacheTool/MNCacheClass.m
@@ -12,17 +12,25 @@
 @implementation MNCacheClass
 
 + (void)mn_saveModel:(id)model key:(NSString *)key{
-
+    
+    //model转json
     NSDictionary *productDic = [model yy_modelToJSONObject];
-
-    [[NSUserDefaults standardUserDefaults] setObject:productDic forKey:key];
+    
+    /**
+     NSUserDefaults支持的数据格式有：NSNumber（Integer、Float、Double），NSString，NSDate，NSArray，NSDictionary，BOOL类型，而我们这里存储的是自定义的model类型-ProductModel，该类型不被支持
+     */
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:productDic];
+    [[NSUserDefaults standardUserDefaults] setObject:data forKey:key];
+    
 }
 
 
 + (id) mn_getSaveModelWithkey:(NSString *)key modelClass:(Class)modelClass{
-    
-    NSDictionary *dict = [[NSUserDefaults standardUserDefaults] objectForKey:key];
-    
+
+    NSData *data = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    NSDictionary *dict = [NSKeyedUnarchiver unarchiveObjectWithData:data];
     return [modelClass yy_modelWithJSON:dict];
 }
 @end
+
+
